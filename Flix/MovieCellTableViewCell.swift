07af5a4b0d646cee5c8e4cb2af5c4844f4131220
nikhilagr <nikhil.agrawal005@gmail.com>
2019-02-07 10:08:44 -0800

    first commit

---
+++ Flix/MovieCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCellTableViewCell.swift
+//  Flix
+//
+//  Created by Nikhil Agrawal on 07/02/19.
+//  Copyright © 2019 Nikhil Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class MovieCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Flix/MoviesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesViewController.swift
+//  Flix
+//
+//  Created by Nikhil Agrawal on 06/02/19.
+//  Copyright © 2019 Nikhil Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
