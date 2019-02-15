38ab54fcaee2906a0eb30b3be75de6ac89a6b9bc
nikhilagr <nikhil.agrawal005@gmail.com>
2019-02-14 19:10:40 -0800

    Adding Part 2

---
+++ Flix/DescriptionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetscriptionViewController.swift
+//  Flix
+//
+//  Created by Nikhil Agrawal on 08/02/19.
+//  Copyright © 2019 Nikhil Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class DetscriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Nikhil Agrawal on 08/02/19.
+//  Copyright © 2019 Nikhil Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+}
